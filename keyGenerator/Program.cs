6b5ad527a8ba057d65fd78e0f2f899d6c3187a84
ChaosRoboticsLab <mikh7272@gmail.com>
2023-12-10 17:09:32 +0300

    Initial commit

---
+++ keyGenerator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string path = "C:\\Users\\Chaos\\RiderProjects\\keyGenerator\\keyGenerator\\wordList.txt"; 
+
+        List<string> words = new List<string>();
+        try
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    words.AddRange(line.Split(' '));
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Ошибка чтения файла:");
+            Console.WriteLine(e.Message);
+            return;
+        }
+
+        Random rnd = new Random();
+        string newWord = "";
+        for (int i = 0; i < 3; i++)
+        {
+            int index = rnd.Next(words.Count);
+            string selectedWord = words[index];
+            newWord += selectedWord.Length >= 3 ? selectedWord.Substring(0, 3) : selectedWord;
+        }
+
+        Console.WriteLine("Пароль: " + newWord);
+    }
+}
